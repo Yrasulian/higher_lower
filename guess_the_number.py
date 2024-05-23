3e977718a9fa0c84874ce8dbade1269393a4ec30
yusef <yusufrfasulian@gmail.com>
2024-05-23 17:24:48 +0200

    writing a guess number game for fun

---
+++ guess_the_number.py
@@ -0,0 +1,40 @@
+import random 
+
+continue_game = True
+
+chosen_number = random.randint(1, 100)
+game_level = input("choos a difficulty level : 'e' for easy 'h' for hard ")
+guessed_number = int(input("please guess a number between 1 and 100 :  "))
+
+
+def compare():
+  global guessed_number
+  global continue_game
+  # while chosen_number != guessed_number:  
+    
+  if chosen_number == guessed_number :
+    return "that was correct" 
+    continue_game = False
+  elif chosen_number < guessed_number :
+    guessed_number = int(input("too much guess a smaller one : "))
+  
+  elif chosen_number > guessed_number:
+    guessed_number = int(input("its too low guess a biger one : "))
+  else:
+   print("you lose your chances")
+
+def hard_game():
+     for i in range(4):
+         compare()
+def easy_game():
+  
+    for i in range(9):
+        compare()
+ 
+def play_game():
+  if game_level == "h":
+     hard_game()
+  elif game_level == "e":
+     easy_game()
+  
+play_game()
